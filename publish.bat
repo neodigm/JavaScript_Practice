4bd055b67b4613869120be6468a609dc51b94f73
Scott C. Krause <3151842+neodigm@users.noreply.github.com>
2021-05-17 16:54:46 -0500

    🚀 JavaScript && TypeScript && Go 🪐

--- publish.bat
+++ publish.bat
@@ -1,5 +1,5 @@
 git status .
 git commit -m "🌴 Creative, Business, and Tech ⚡"
-git commit -m "🌴 Creative, Business, and Tech ⚡"
+git commit -m "🚀 JavaScript && TypeScript && Go 🪐"
 git branch
 git push origin gh-pages
