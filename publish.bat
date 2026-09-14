342945ed4ccd94001df15caec444f605b962c9c0
Scott C. Krause <3151842+neodigm@users.noreply.github.com>
2026-09-14 09:26:35 -0500

    MCP AI Agents Chat UX PWA 3D ThreeJS LLM ✨ HTMX Rust WASM 🌶️ webMCP
    MCP AI Agents Chat UX PWA 3D ThreeJS LLM ✨ HTMX Rust WASM 🌶️ webMCP

--- publish.bat
+++ publish.bat
@@ -1,6 +1,6 @@
 git status .
 git add .
-git commit -m "🌴 Creative, Business, and Tech ⚡"
+git commit -m "MCP AI Agents Chat UX PWA 3D ThreeJS LLM ✨ HTMX Rust WASM 🌶️ webMCP"
 git commit -m "🚀 JavaScript && TypeScript && Go 🪐"
 machvive -x "MCP AI Agents Chat UX PWA 3D Lit ThreeJS Vue ✨ HTMX Rust WASM"
 git branch
